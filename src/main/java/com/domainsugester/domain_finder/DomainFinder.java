63649ef547e47e3f33742ccb0ef898202ccb5022
Vitor N <vitornc31@gmail.com>
2026-05-11 17:28:45 -0300

    build: initializes the maven project

---
+++ src/main/java/com/domainsugester/domain_finder/DomainFinder.java
@@ -0,0 +1,15 @@
+package com.domainsugester.domain_finder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomainFinder {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(DomainFinder.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/domainsugester/domain_finder/DomainFinderTests.java
@@ -0,0 +1,13 @@
+package com.domainsugester.domain_finder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomainFinderTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
