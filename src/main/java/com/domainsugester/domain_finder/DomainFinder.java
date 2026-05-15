61d8dd97d4a9440bd88d189add49decacaf71e34
Vitor Natal Cordeiro <vitor.natal@pucpr.edu.br>
2026-05-15 16:45:05 -0300

    build: removes spring JPA dependency and add redis configuration to application.yaml

--- src/main/java/com/domainsugester/domain_finder/DomainFinder.java
+++ src/main/java/com/domainsugester/domain_finder/DomainFinder.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DomainFinder {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(DomainFinder.class, args);
 	}
 
