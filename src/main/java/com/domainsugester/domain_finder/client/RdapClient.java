2982f224c721a5b156bf1f5350695985f418b982
Vitor Natal <vitornc31@gmail.com>
2026-06-02 17:32:53 -0300

    refactor: rename FeignClient name and classname

--- src/main/java/com/domainsugester/domain_finder/client/RdapClient.java
+++ src/main/java/com/domainsugester/domain_finder/client/RdapClient.java
@@ -4,13 +4,12 @@ import com.domainsugester.domain_finder.dto.external.ApifyDomainResponse;
 import com.domainsugester.domain_finder.dto.request.DomainRequest;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
-@FeignClient(name="ApifyClient", url="${RDAP_BASE_URL}")
-public interface RdapDomainCheckerClient {
+@FeignClient(name="RdapClient", url="${RDAP_BASE_URL}")
+public interface RdapClient {
 
     @GetMapping("/domain")
     List<ApifyDomainResponse> getDomain(@RequestBody DomainRequest domainRequest);
