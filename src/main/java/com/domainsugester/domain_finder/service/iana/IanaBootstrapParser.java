f428e936e5574ca91799af32c645b465d0faaf8a
Vitor <vitornc31@gmail.com>
2026-06-02 18:50:54 -0300

    feat: add iana parser class

---
+++ src/main/java/com/domainsugester/domain_finder/service/iana/IanaBootstrapParser.java
@@ -0,0 +1,19 @@
+package com.domainsugester.domain_finder.service.iana;
+
+import com.domainsugester.domain_finder.dto.external.iana.IanaBootstrapResponse;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class IanaBootstrapParser {
+    public Map<String, String> parseRawBootstrap(IanaBootstrapResponse bootstrap){
+        Map<String, String> map = new HashMap<>();
+        bootstrap.services().forEach(pair -> {
+            String rdapUrl = pair.get(1).get(0);
+            pair.get(0).forEach(tld -> map.put(tld, rdapUrl));
+        });
+        return map;
+    }
+}
