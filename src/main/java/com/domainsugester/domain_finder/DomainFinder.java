4fb74e99714b5eb57ab37b50aaf255999e0d2f6f
Vitor <vitornc31@gmail.com>
2026-06-03 17:04:44 -0300

    build: enable retry and feign client

--- src/main/java/com/domainsugester/domain_finder/DomainFinder.java
+++ src/main/java/com/domainsugester/domain_finder/DomainFinder.java
@@ -2,8 +2,9 @@ package com.domainsugester.domain_finder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
 
 @EnableRetry
 @SpringBootApplication
