e995f4af1223b1854456a24500822f9e10c7883e
Vitor <vitornc31@gmail.com>
2026-06-04 12:55:44 -0300

    feat: add bootstrap controller

---
+++ src/main/java/com/domainsugester/domain_finder/controller/IanaBootstrapController.java
@@ -0,0 +1,27 @@
+package com.domainsugester.domain_finder.controller;
+import com.domainsugester.domain_finder.service.iana.IanaBootstrapService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/admin/ianaBootstrap")
+@RequiredArgsConstructor
+public class IanaBootstrapController {
+    private final IanaBootstrapService bootstrapService;
+
+    @GetMapping
+    public ResponseEntity<IanaBootstrapController> getCachedBootstrap(){
+        return ResponseEntity.ok().body(null);
+
+    }
+
+    @PostMapping("/refresh")
+    public ResponseEntity<Void> refreshIanaBootStrap(){
+        bootstrapService.refreshIanaBootStrap();
+        return ResponseEntity.accepted().build();
+    }
+}
