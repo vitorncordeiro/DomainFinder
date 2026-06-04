52641718979ae26ab876eb4485e5e090aa94985f
Vitor <vitornc31@gmail.com>
2026-06-04 12:53:30 -0300

    refactor: add description & version to cache and add fetchBootstrap method

--- src/main/java/com/domainsugester/domain_finder/cache/IanaBootstrapCacheService.java
+++ src/main/java/com/domainsugester/domain_finder/cache/IanaBootstrapCacheService.java
@@ -1,20 +1,57 @@
 package com.domainsugester.domain_finder.cache;
 
-import com.domainsugester.domain_finder.dto.external.iana.IanaBootstrapResponse;
+import com.domainsugester.domain_finder.dto.response.CacheBootstrapResponse;
+import com.domainsugester.domain_finder.mapper.CacheBootstrapMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.Cursor;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ScanOptions;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Service;
 
 import java.time.Duration;
+import java.util.*;
 
 @Service
 @RequiredArgsConstructor
 public class IanaBootstrapCacheService {
     private final RedisTemplate<String, Object> redisTemplate;
+    private final ValueOperations<String, Object> ops = redisTemplate.opsForValue();
+
 
     public void save(String key, String value, Duration ttl){
-        ValueOperations<String, Object> ops = redisTemplate.opsForValue();
         ops.set(key, value, ttl);
+    }
+
+    public Optional<String> fetchTldServer(String tld){
+        String tldServer = ops.get(tld).toString();
+        return Optional.ofNullable(tldServer);
+    }
+    public CacheBootstrapResponse fetchBootstrap(){
+        Set<String> keys = scanKeys("iana:tld:*");
+        Map<String, String> map = new HashMap<>();
+        keys.forEach(key -> {
+            map.put(key, ops.get(key).toString());
+        });
+        Optional<String> description = Optional.ofNullable(ops.get("iana:description").toString());
+        Optional<String> version = Optional.ofNullable(ops.get("iana:version").toString());
+
+        return CacheBootstrapMapper.toCacheResponse(map, description.toString(), version.toString());
+    }
+    private Set<String> scanKeys(String keyPrefix){
+        Set<String> keys = new HashSet<>();
+
+        ScanOptions options = ScanOptions.scanOptions()
+                .match(keyPrefix)
+                .count(100)
+                .build();
+        try (Cursor<byte[]> cursor = redisTemplate.executeWithStickyConnection(
+                connection -> connection.keyCommands().scan(options))) {
+
+            while (cursor.hasNext()) {
+                keys.add(new String(cursor.next()));
+            }
+        }
+        return keys;
     }
 }
--- src/main/java/com/domainsugester/domain_finder/service/iana/IanaBootstrapParser.java
+++ src/main/java/com/domainsugester/domain_finder/service/iana/IanaBootstrapParser.java
@@ -10,6 +10,8 @@ import java.util.Map;
 public class IanaBootstrapParser {
     public Map<String, String> parseRawBootstrap(IanaBootstrapResponse bootstrap){
         Map<String, String> map = new HashMap<>();
+        map.put("iana:description", bootstrap.description());
+        map.put("iana:version", bootstrap.version());
         bootstrap.services().forEach(pair -> {
             String rdapUrl = pair.get(1).get(0);
             pair.get(0).forEach(tld -> map.put(("iana:tld:" + tld), rdapUrl));
