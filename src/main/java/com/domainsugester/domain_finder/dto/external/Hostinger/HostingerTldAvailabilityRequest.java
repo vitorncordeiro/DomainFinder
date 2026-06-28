d1a3d187aa94154bfff0e5dd3849a411ed9aee05
vitornc <vitornc31@gmail.com>
2026-06-28 20:21:44 -0300

    build: add feign jackson dependecy

--- src/main/java/com/domainsugester/domain_finder/dto/external/Hostinger/HostingerTldAvailabilityRequest.java
+++ src/main/java/com/domainsugester/domain_finder/dto/external/Hostinger/HostingerTldAvailabilityRequest.java
@@ -1,10 +1,13 @@
 package com.domainsugester.domain_finder.dto.external.Hostinger;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.util.Set;
 
 public record HostingerTldAvaliabilityRequest(
     String domain,
     Set<String> tlds,
+    @JsonProperty("with_alternatives")
     Boolean withAlternatives
 ) {
 }
