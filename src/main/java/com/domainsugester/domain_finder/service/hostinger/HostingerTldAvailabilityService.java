651fbe014815acca894e051ea25c78f100e4fbe6
vitornc <vitornc31@gmail.com>
2026-06-30 21:25:09 -0300

    feat: adds hostinger tld list caller with playwright headless browser

--- src/main/java/com/domainsugester/domain_finder/service/hostinger/HostingerTldAvailabilityService.java
+++ src/main/java/com/domainsugester/domain_finder/service/hostinger/HostingerTldAvailabilityService.java
@@ -1,97 +1,45 @@
 package com.domainsugester.domain_finder.service.hostinger;
 
-import com.domainsugester.domain_finder.client.HostingerClient;
-import com.domainsugester.domain_finder.dto.external.Hostinger.HostingerTldAvailabilityRequest;
-import com.domainsugester.domain_finder.dto.external.Hostinger.HostingerTldAvaliabilityResponse;
-import lombok.RequiredArgsConstructor;
+import com.microsoft.playwright.*;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import java.util.*;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-import java.util.concurrent.ThreadLocalRandom;
-import java.util.stream.Collectors;
-
+@Slf4j
 @Service
-@RequiredArgsConstructor
 public class HostingerTldAvailabilityService {
-    private final HostingerClient hostingerClient;
-    private static final String LETTERS = "abcdefghijklmnopqrstuvwxyz";
-    private static final int DOMAIN_LENGTH = 16;
 
-    public Set<String> getAvaliableTldSet(Set<String> tldSet) {
-        tldSet = filterTlds(tldSet);
-        int batchSize = (tldSet.size() + 2) / 3;
-        var batches = splitIntoBatches(tldSet, batchSize);
-        Set<String> avaliableTlds = new HashSet<>();
-        for(int i =0; i < 3; i++){
-            Set<HostingerTldAvaliabilityResponse> responses = executeRequestBatch(batches, generateDomain());
-            avaliableTlds.addAll(extractAvailableTldSet(responses));
-        }
-        return avaliableTlds;
-    }
+    @Value("${playwright.server.url:}")
+    private String playwrightServerUrl;
 
-    private Set<String> filterTlds(Set<String> tldSet){
-        return  tldSet.stream()
-                .map(tld -> tld.substring(9))
-                .filter(tld -> !tld.startsWith("xn--"))
-                .collect(Collectors.toSet());
-    }
-    private Set<HostingerTldAvaliabilityResponse> executeRequestBatch(List<Set<String>> subSets, String domain){
-        List<Set<HostingerTldAvaliabilityResponse>> responseBatches = subSets.parallelStream()
-                .map(subSet -> {
-                    HostingerTldAvailabilityRequest request = new HostingerTldAvailabilityRequest(
-                            domain,
-                            subSet,
-                            false
-                    );
-                    System.out.println("Request: " + request);
+    public String getAvailableTlds() {
+        try (Playwright playwright = Playwright.create(
+                new Playwright.CreateOptions()
+                        .setEnv(Map.of("PLAYWRIGHT_SKIP_BROWSER_DOWNLOAD", "1"))
+        )) {
+            Browser browser = playwright.chromium().connect(
+                    playwrightServerUrl,
+                    new BrowserType.ConnectOptions().setTimeout(30_000)
+            );
 
-                    var a = hostingerClient.getHostingerTldAvaliabilities(request);
-                    System.out.println(a);
-
-                    return hostingerClient.getHostingerTldAvaliabilities(request);
-                })
-                .toList();
-        return responseBatches.stream()
-                .flatMap(Set::stream)
-                .collect(Collectors.toSet());
-    }
-    private List<Set<String>> splitIntoBatches(Set<String> tldSet, int batchSize) {
-
-        List<String> tlds = new ArrayList<>(tldSet);
-        List<Set<String>> batches = new ArrayList<>();
-
-        for (int i = 0; i < tlds.size(); i += batchSize) {
-
-            batches.add(
-                    new HashSet<>(
-                            tlds.subList(
-                                    i,
-                                    Math.min(i + batchSize, tlds.size())
-                            )
+            Page page = browser.newPage();
+            Response response = page.waitForResponse(
+                    r -> r.url().contains("available-tlds-by-theme"),
+                    () -> page.navigate(
+                            "https://www.hostinger.com/domain-name-results" +
+                                    "?from=homepage&domain=dawda.com"
                     )
             );
-        }
-        return batches;
-    }
 
-    private String generateDomain(){
-        StringBuilder domain = new StringBuilder();
-        for (int i = 0; i < DOMAIN_LENGTH; i++) {
-            int randomIndex = ThreadLocalRandom.current().nextInt(LETTERS.length());
-            domain.append(LETTERS.charAt(randomIndex));
+            String result = response.text();
+            log.info("TLDs Successfully catched. Status: {}", response.status());
+
+            browser.close();
+            return result;
+
+        } catch (Exception e) {
+            throw new RuntimeException("Error while capturing TLDs", e);
         }
-        return domain.toString();
-    }
-    private Set<String> extractAvailableTldSet(Set<HostingerTldAvaliabilityResponse> responses){
-        return responses.stream()
-                .filter(HostingerTldAvaliabilityResponse::isAvailable)
-                .map(avaliable -> {
-                    String domain = avaliable.domain();
-                    return domain.substring(domain.indexOf('.') + 1);
-                })
-                .collect(Collectors.toSet());
     }
 }
