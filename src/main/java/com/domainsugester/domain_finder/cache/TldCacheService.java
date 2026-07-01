be3412c204212a3ce0a9e74095c796f93908cf07
vitornc <vitornc31@gmail.com>
2026-06-30 22:23:43 -0300

    refactor: turn tld cache service into interface

--- src/main/java/com/domainsugester/domain_finder/cache/TldCacheService.java
+++ src/main/java/com/domainsugester/domain_finder/cache/TldCacheService.java
@@ -1,18 +1,8 @@
 package com.domainsugester.domain_finder.cache;
 
 import com.domainsugester.domain_finder.dto.response.CacheBootstrapResponse;
-import com.domainsugester.domain_finder.mapper.CacheBootstrapMapper;
-import lombok.RequiredArgsConstructor;
-import org.springframework.data.redis.core.Cursor;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.ScanOptions;
-import org.springframework.data.redis.core.ValueOperations;
-import org.springframework.stereotype.Service;
 
-import java.time.Duration;
-import java.util.*;
-
-public interface TldBootstrapCacheService{
+public interface TldCacheService {
 
     void save(String key, String value);
     CacheBootstrapResponse fetchBootstrap();
